5f913015b38c191f04ee82208a832166668c4c05
Brandon <brandnlholl@gmail.com>
2023-09-27 20:40:48 +0000

    Nextjs Init

---
+++ dayssincelastwarthunderleak/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="w-screen h-screen flex content-center justify-center">
+      <h1 className="font-bold text-9xl">0</h1>
+    </main>
+  );
+}
