3b7c0de0abef016e12a36543f1dc1991295be761
Brandon <brandnlholl@gmail.com>
2023-09-28 00:17:04 +0000

    Favicon and meta data

--- dayssincelastwarthunderleak/app/page.tsx
+++ dayssincelastwarthunderleak/app/page.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen flex content-center justify-center">
-      <h1 className="font-bold text-9xl">0</h1>
+    <main className="w-screen h-screen flex flex-col justify-center">
+      <div className="flex justify-center">
+        <h1 className="font-bold text-[16rem]">0</h1>
+      </div>
     </main>
   );
 }
+ 
